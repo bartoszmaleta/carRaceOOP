f0ff3d9c8b0a503b8731aba571275f095ec23df8
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-26 23:58:45 +0100

    Add Car + Helpers

---
+++ src/app/Car.java
@@ -0,0 +1,9 @@
+// package app;
+
+// public class Car extends Vehicle {
+
+//     public Car() {
+//         this.normalnSpeed = Util.getRandomNumber(80, 110);
+//         this.name = Util.generateCarName();
+//     }
+// }
